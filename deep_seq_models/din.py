2015326a0eba6b851c2ced204e389b196eaab5ec
goldandrabbit <goldandrabbit@foxmail.com>
2020-07-05 22:36:14 +0800

    update din models

--- deep_seq_models/din.py
+++ deep_seq_models/din.py
@@ -1,37 +1,29 @@
+import math
 import tensorflow as tf
 from utils.ama_ele_feat_config import build_ama_ele_columns
 
 
-def attention_layer(querys,keys,keys_id):
-    """
-        queries:     [Batchsize, 1, embedding_size]
-        keys:        [Batchsize, max_seq_len, embedding_size]  max_seq_len is the number of keys(e.g. number of clicked creativeid for each sample)
-        keys_id:     [Batchsize, max_seq_len]
-    """
-    keys_length = tf.shape(keys)[1]  # padded_dim
-    embedding_size = querys.get_shape().as_list()[-1]
-    keys = tf.reshape(keys,shape=[-1,keys_length,embedding_size])
-    querys = tf.reshape(tf.tile(querys,[1,keys_length,1]),shape=[-1,keys_length,embedding_size])
-
-    net = tf.concat([keys,keys - querys,querys,keys * querys],axis=-1)
-    for units in [32,16]:
-        net = tf.layers.dense(net,units=units,activation=tf.nn.relu)
-    att_wgt = tf.layers.dense(net,units=1,activation=tf.sigmoid)  # shape(batch_size, max_seq_len, 1)
-    outputs = tf.reshape(att_wgt,shape=[-1,1,keys_length],name="weight")  # shape(batch_size, 1, max_seq_len)
-
-    scores = outputs
-    # key_masks = tf.expand_dims(tf.cast(keys_id > 0, tf.bool), axis=1)  # shape(batch_size, 1, max_seq_len) we add 0 as padding
-    # tf.not_equal(keys_id, '0')  如果改成str
-    key_masks = tf.expand_dims(tf.not_equal(keys_id,'0'),axis=1)
-    paddings = tf.ones_like(scores) * (-2 ** 32 + 1)
-    scores = tf.where(key_masks,scores,paddings)
-    scores = scores / (embedding_size ** 0.5)  # scale
-    scores = tf.nn.softmax(scores)
-    outputs = tf.matmul(scores,keys)  # (batch_size, 1, embedding_size)
-    outputs = tf.reduce_sum(outputs,1,name="attention_embedding")  # (batch_size, embedding_size)
-    return outputs
-
 def din_model_fn(features, labels, mode, params):
+    def _attention_layer(queries,keys):
+        """
+        queries: (batch_size * emb_size)
+        keys:    (batch_size * seq_size * emb_size)
+        """
+        emb_size = queries.get_shape().as_list()[-1]                                # get emb_size
+        seq_size = keys.get_shape().as_list()[1]                                    # get seq_size
+        queries = tf.reshape(tf.tile(queries,[1,seq_size]),
+                             [-1,seq_size,emb_size])                                # tile queries as same as keys shape, then reshape
+        att_unit = tf.concat([queries,keys,queries - keys,queries * keys], axis=-1) # batch_size * seq_size * [4 * emb_size]
+        unit_layer_1 = tf.layers.dense(att_unit,80,activation=tf.nn.sigmoid)
+        unit_layer_2 = tf.layers.dense(unit_layer_1,40,activation=tf.nn.sigmoid)
+        unit_layer_3 = tf.layers.dense(unit_layer_2,1,activation=None)              # batch_size * seq_size * 1
+        outputs = tf.reshape(unit_layer_3,[-1,1,seq_size])                          # batch_size * 1 * seq_size
+        outputs = outputs / (math.sqrt(emb_size))                                   # scale
+        outputs = tf.nn.softmax(outputs)                                            # softmax
+        outputs = tf.matmul(outputs, keys)                                          # weighted sum: batch_size * ((1 * seq_size) * (seq_size * emb_size)) => batch_size * 1 * emb_size
+        outputs = tf.reshape(outputs,[-1, emb_size])
+        return outputs
+
     emb_feat_columns, emb_field_size = build_ama_ele_columns()
     emb_input_layer = tf.feature_column.input_layer(features=features, feature_columns=emb_feat_columns)
 
@@ -45,29 +37,20 @@ def din_model_fn(features, labels, mode, params):
         seq_items = tf.string_to_hash_bucket_fast(features['seq'], 200000)
         target_item = tf.string_to_hash_bucket_fast(features['item_id'], 200000)
         item_emb_matrix = tf.get_variable(name='item_emb_table', dtype=tf.float32, shape=[200000, 8])
-        seq_items_emb = tf.nn.embedding_lookup(item_emb_matrix, seq_items)
-        target_item_emb = tf.nn.embedding_lookup(item_emb_matrix, target_item)
-        print('seq_items_emb')
-        print(seq_items_emb.get_shape())
-        print('target_item_emb')
-        print(target_item_emb.get_shape())
+        seq_items_emb = tf.nn.embedding_lookup(item_emb_matrix, seq_items)      # keys:    (batch_size * seq_size * emb_size)
+        target_item_emb = tf.nn.embedding_lookup(item_emb_matrix, target_item)  # queries: (batch_size * emb_size)
 
         seq_cates = tf.string_to_hash_bucket_fast(features['seq_cate'], 200000)
         target_cate = tf.string_to_hash_bucket_fast(features['item_cate'], 200000)
         cate_emb_matrix = tf.get_variable(name='cate_emb_table', dtype=tf.float32, shape=[200000, 8])
         seq_cates_emb = tf.nn.embedding_lookup(cate_emb_matrix, seq_cates)
         target_cate_emb = tf.nn.embedding_lookup(cate_emb_matrix, target_cate)
-        print('seq_cates_emb')
-        print(seq_cates_emb.get_shape())
-        print('target_cate_emb')
-        print(target_cate_emb.get_shape())
 
-        # seq_items_attention = attention_layer(target_item_emb, seq_items_emb, features['seq'])
-        # seq_cates_attention = attention_layer(target_cate_emb, seq_cates_emb, features['seq_cate'])
+        seq_items_attention = _attention_layer(target_item_emb, seq_items_emb)
+        seq_cates_attention = _attention_layer(target_cate_emb, seq_cates_emb)
 
     with tf.name_scope('concat'):
-        # m_layer = tf.concat([seq_items_attention, seq_cates_attention, deep_output_layer], 1)
-        m_layer = tf.concat([deep_output_layer], 1)
+        m_layer = tf.concat([deep_output_layer, seq_items_attention, seq_cates_attention], axis=1)
         o_layer = tf.layers.dense(inputs=m_layer, units=1, activation=None, use_bias=True)
 
     with tf.name_scope('logit'):
@@ -75,19 +58,16 @@ def din_model_fn(features, labels, mode, params):
         predictions = tf.cast((o_prob > 0.5), tf.float32)
 
     labels = tf.cast(labels, tf.float32, name='true_label')
-    # define loss
     loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=o_layer))
-    # evaluation
     accuracy = tf.metrics.accuracy(labels, predictions)
     auc = tf.metrics.auc(labels, predictions)
     my_metrics = {
         'accuracy': tf.metrics.accuracy(labels, predictions),
-        'auc': tf.metrics.auc(labels, predictions)
+        'auc':      tf.metrics.auc(labels, predictions)
     }
     tf.summary.scalar('accuracy', accuracy[1])
     tf.summary.scalar('auc', auc[1])
 
-    # define train_op
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999)
     train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
 
