e655d7b998f2b802d7592fd85b4f3f8821d34e43
goldandrabbit <goldandrabbit@foxmail.com>
2020-08-09 21:27:59 +0800

    update config

--- train_seq_v2.py
+++ train_seq_v2.py
@@ -32,10 +32,10 @@ def seq_input_fn(data_file, is_predict, config):
 def build_seq_estimator(model_dir):
     run_config = tf.estimator.RunConfig().replace(
         session_config=tf.ConfigProto(device_count={'GPU': 0}),
-        save_summary_steps=50,
-        log_step_count_steps=50,
+        save_summary_steps=200,
+        log_step_count_steps=200,
         keep_checkpoint_max=3,
-        save_checkpoints_steps=50
+        save_checkpoints_steps=1000
     )
     with open('data/amazon/remap.pkl','rb') as f:
         _ = pickle.load(f)
